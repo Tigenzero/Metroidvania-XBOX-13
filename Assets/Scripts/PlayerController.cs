d2038bbfd311f0097541cd5ef2a314099d416754
Matt Hull <Tigenzero@gmail.com>
2022-07-29 09:47:45 -0400

    player damage effect and death, doors and scene changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -53,109 +53,116 @@ public class PlayerController : MonoBehaviour
     // Ball Bomb
     public Transform bombPoint;
     public GameObject bomb;
+
+    public bool canMove;
     
 
     // Start is called before the first frame update
     void Start()
     {
         abilities = GetComponent<PlayerAbilityTracker>();
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (canMove) {
     
-    	if (dashRechargeCounter > 0) {
-    		dashRechargeCounter -= Time.deltaTime;
-    	} else {
-    		if (Input.GetButtonDown("Fire2") && standing.activeSelf && abilities.canDash) {
-            dashCounter = dashTime;
+            if (dashRechargeCounter > 0) {
+                dashRechargeCounter -= Time.deltaTime;
+            } else {
+                if (Input.GetButtonDown("Fire2") && standing.activeSelf && abilities.canDash) {
+                dashCounter = dashTime;
 
-            ShowAfterImage();
-        	}
-    	}
-
-        
-        if (dashCounter > 0) {
-            dashCounter = dashCounter - Time.deltaTime;
-
-            theRB.velocity = new Vector2(dashSpeed * transform.localScale.x, theRB.velocity.y);
-
-            afterImageCounter -= Time.deltaTime;
-            if (afterImageCounter <= 0) {
                 ShowAfterImage();
+                }
+            }
+
+            
+            if (dashCounter > 0) {
+                dashCounter = dashCounter - Time.deltaTime;
+
+                theRB.velocity = new Vector2(dashSpeed * transform.localScale.x, theRB.velocity.y);
+
+                afterImageCounter -= Time.deltaTime;
+                if (afterImageCounter <= 0) {
+                    ShowAfterImage();
+                }
+                
+                dashRechargeCounter = waitAfterDashing;
+
+                Collider2D[] objectsToRemove = Physics2D.OverlapCircleAll(transform.position, dashRange, whatIsDashDestructible);
+
+                if (objectsToRemove.Length > 0) {
+                    foreach (Collider2D item in objectsToRemove)
+                    {
+                        Destroy(item.gameObject);
+                    }
+                }
+            }
+            else {
+                moveCharacter(Input.GetAxisRaw("Horizontal"));
             }
             
-            dashRechargeCounter = waitAfterDashing;
 
-            Collider2D[] objectsToRemove = Physics2D.OverlapCircleAll(transform.position, dashRange, whatIsDashDestructible);
 
-            if (objectsToRemove.Length > 0) {
-                foreach (Collider2D item in objectsToRemove)
+            // Am I on the Ground?
+            isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
+
+
+            // Jumping
+            if(Input.GetButtonDown("Jump") && (isOnGround || (canDoubleJump && abilities.canDoubleJump))) {
+                if (isOnGround) {
+                    canDoubleJump = true;
+                } else {
+                    canDoubleJump = false;
+                    anim.SetTrigger("doubleJump");
+                }
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+
+
+
+            if (Input.GetButtonDown("Fire1")) {
+                if (standing.activeSelf)
                 {
-                    Destroy(item.gameObject);
+                    Instantiate(shotToFire, ShotPoint.position, ShotPoint.rotation).moveDir = new Vector2(transform.localScale.x, 0f);
+                    anim.SetTrigger("shotFired");
+                } else if(ball.activeSelf)
+                {
+                    Instantiate(bomb, bombPoint.position, bombPoint.rotation);
                 }
+                
             }
-        }
-        else {
-            moveCharacter(Input.GetAxisRaw("Horizontal"));
-        }
-        
 
 
-        // Am I on the Ground?
-        isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
-
-
-        // Jumping
-        if(Input.GetButtonDown("Jump") && (isOnGround || (canDoubleJump && abilities.canDoubleJump))) {
-            if (isOnGround) {
-                canDoubleJump = true;
-            } else {
-                canDoubleJump = false;
-                anim.SetTrigger("doubleJump");
-            }
-            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
-        }
-
-
-
-        if (Input.GetButtonDown("Fire1")) {
-            if (standing.activeSelf)
-            {
-                Instantiate(shotToFire, ShotPoint.position, ShotPoint.rotation).moveDir = new Vector2(transform.localScale.x, 0f);
-                anim.SetTrigger("shotFired");
-            } else if(ball.activeSelf)
-            {
-                Instantiate(bomb, bombPoint.position, bombPoint.rotation);
-            }
-            
-        }
-
-
-        // ball mode
-        if (!ball.activeSelf) {
-            //Are we pressing down?
-            if (Input.GetAxisRaw("Vertical") < -.9f) {
-                ballCounter -= Time.deltaTime;
-                if (ballCounter <= 0) {
-                    ball.SetActive(true);
-                    standing.SetActive(false);
+            // ball mode
+            if (!ball.activeSelf) {
+                //Are we pressing down?
+                if (Input.GetAxisRaw("Vertical") < -.9f) {
+                    ballCounter -= Time.deltaTime;
+                    if (ballCounter <= 0) {
+                        ball.SetActive(true);
+                        standing.SetActive(false);
+                    }
+                } else {
+                    ballCounter = waitToBall;
                 }
             } else {
-                ballCounter = waitToBall;
+                //Are we pressing up?
+                if (Input.GetAxisRaw("Vertical") > .9f) {
+                    ballCounter -= Time.deltaTime;
+                    if (ballCounter <= 0) {
+                        ball.SetActive(false);
+                        standing.SetActive(true);
+                    }
+                } else {
+                    ballCounter = waitToBall;
+                }
             }
         } else {
-            //Are we pressing up?
-            if (Input.GetAxisRaw("Vertical") > .9f) {
-                ballCounter -= Time.deltaTime;
-                if (ballCounter <= 0) {
-                    ball.SetActive(false);
-                    standing.SetActive(true);
-                }
-            } else {
-                ballCounter = waitToBall;
-            }
+            theRB.velocity = Vector2.zero;
         }
 
 
--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -8,13 +8,29 @@ public class PlayerHealthController : MonoBehaviour
 
     // when first activated, or reactivated
     // occurs before start function
+    // Keeps gameObject consistent between scenes.
     private void Awake() {
-        instance = this;
+        if (instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+        
     }
 
     // [HideInInspector]
     public int currentHealth;
     public int maxHealth;
+
+    // Reacting to Player Damage
+    public float invincibilityLength;
+    private float invincCounter;
+
+    public float flashLength;
+    private float flashCounter;
+
+    public SpriteRenderer[] playerSprites;
 
     // Start is called before the first frame update
     void Start()
@@ -27,18 +43,57 @@ public class PlayerHealthController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (invincCounter > 0) {
+            invincCounter -= Time.deltaTime;
+
+            flashCounter -= Time.deltaTime;
+            if (flashCounter < 0) {
+                foreach (SpriteRenderer sr in playerSprites) {
+                    sr.enabled = !sr.enabled;
+                }
+                flashCounter = flashLength;
+            }
+
+            if (invincCounter <= 0) {
+                foreach (SpriteRenderer sr in playerSprites) {
+                    sr.enabled = true;
+                }
+                flashCounter = 0f;
+            }
+        }
     }
 
     public void DamagePlayer(int damageAmount) {
-        currentHealth -= damageAmount;
+        if (invincCounter <= 0){
+            currentHealth -= damageAmount;
 
-        if(currentHealth <= 0) {
-            currentHealth = 0;
+            // Death
+            if(currentHealth <= 0) {
+                currentHealth = 0;
 
-            gameObject.SetActive(false);
+                // gameObject.SetActive(false);
+
+                RespawnController.instance.Respawn();
+            }
+            // Not dead? Temp Invincibility
+            else {
+                invincCounter = invincibilityLength;
+            }
+
+            UIController.instance.UpdateHealth(currentHealth, maxHealth);
         }
+    }
 
+    public void FillHealth(){
+        currentHealth = maxHealth;
+        UIController.instance.UpdateHealth(currentHealth, maxHealth);
+    }
+
+    public void HealPlayer(int healAmount) {
+        currentHealth += healAmount;
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
         UIController.instance.UpdateHealth(currentHealth, maxHealth);
     }
 }
